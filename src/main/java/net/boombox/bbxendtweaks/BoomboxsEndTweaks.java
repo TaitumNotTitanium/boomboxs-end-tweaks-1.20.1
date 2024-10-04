447fb0ef03b1e3489c8bfd7e8d72684327930d4b
R3ggo <90398030+R3ggo@users.noreply.github.com>
2024-10-03 20:25:06 -0400

    Initial commit

---
+++ src/main/java/net/boombox/bbxendtweaks/BoomboxsEndTweaks.java
@@ -0,0 +1,16 @@
+package net.boombox.bbxendtweaks;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BoomboxsEndTweaks implements ModInitializer {
+	public static final String MOD_ID = "bbxendtweaks";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/boombox/bbxendtweaks/BoomboxsEndTweaksClient.java
@@ -0,0 +1,11 @@
+package net.boombox.bbxendtweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BoomboxsEndTweaksClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
